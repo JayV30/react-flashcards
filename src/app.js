3c7595eee827a2bcbe77b0ce3cb64a74c24997c3
Jason Karlsven <jay@jpkarlsven.com>
2017-04-20 16:32:37 -0400

    move dist in gh-pages to root

--- src/app.js
+++
@@ -1,7 +0,0 @@
-import 'babel-polyfill'; // emulates full ES6 in broswer
-import React from 'react';
-import { render } from 'react-dom';
-
-import Main from './components/class/main';
-
-render(<Main />, document.getElementById('app'));
